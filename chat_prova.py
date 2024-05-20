485c3fd020baae01717ad59e9e851f61efd80dab
Michele Silvani <156786714+MicheleS14@users.noreply.github.com>
2024-05-20 10:41:02 +0200

    aggiunti i file chat_prova.py e func.py
    i file aggiungono le funzioni di registrazione e di login

---
+++ chat_prova.py
@@ -0,0 +1,60 @@
+import redis
+import datetime
+import func
+
+
+# Connessione a Redis
+r = redis.Redis(host='redis-16036.c250.eu-central-1-1.ec2.redns.redis-cloud.com', port=16036, db=0, username='default', password='69Fa488VqsGKuseTkFy5uwVlupgDBF2V')
+
+if __name__ == '__main__':
+    utente_corrente = None
+    
+    while True:
+        print("\n*** Benvenuto nella chat ***")
+        print("1. Login")
+        print("2. Registrazione")
+        print("0. Esci")
+        
+        scelta = input('scelta: ')
+        
+        if scelta == "1":
+            utente_corrente = func.login()
+            if utente_corrente:
+                break
+        elif scelta == "2":
+            func.registrazione()
+        elif scelta == "0":
+            print("Grazie per aver utilizzato la chat. Arrivederci!")
+            break
+        else:
+            print("Scelta non valida. Riprova.")
+
+    if utente_corrente:
+        while True:
+            print("\n*** Benvenuto nella chat ***")
+            print("1. Ricerca utenti")
+            print("2. Aggiungi contatto")
+            print("3. Imposta modalità Do Not Disturb")
+            print("4. Invia messaggio")
+            print("5. Leggi chat")
+            print("0. Esci")
+
+            scelta = input("Scelta: ")
+
+            if scelta == "1":
+                func.ricerca_utenti()
+            elif scelta == "2":
+                func.aggiungi_contatto(utente_corrente)
+            elif scelta == "3":
+                func.impostazione_dnd(utente_corrente)
+            elif scelta == "4":
+                func.invia_mess(utente_corrente)
+            elif scelta == "5":
+                func.leggi_mess(utente_corrente)
+            elif scelta == "0":
+                print("Grazie per aver utilizzato la chat. Arrivederci!")
+                break
+            else:
+                print("Scelta non valida. Riprova.")
+    else:
+        print("Impossibile accedere alla chat. Riprova.")
---
+++ func.py
@@ -0,0 +1,49 @@
+import redis
+import datetime
+
+
+
+# Connessione a Redis
+r = redis.Redis(host='redis-16036.c250.eu-central-1-1.ec2.redns.redis-cloud.com', port=16036, db=0, username='default', password='69Fa488VqsGKuseTkFy5uwVlupgDBF2V')
+
+def registrazione():
+    username = input("Inserisci il nome utente: ")
+    password = input("Inserisci la password: ")
+    
+    # Controllo se l'utente esiste già
+    if r.exists(username):
+        print("Utente già registrato.")
+    else:
+        # Salvo le informazioni dell'utente in Redis
+        r.hset(username, "password", password)
+        r.hset(username, "dnd", "False")
+        print("Registrazione completata.")
+
+# Funzione per effettuare il login
+def login():
+    username = input("Inserisci il nome utente: ")
+    password = input("Inserisci la password: ")
+    
+    # Controllo le credenziali dell'utente
+    stored_password = r.hget(username, "password")
+    if stored_password and stored_password.decode() == password:
+        print("Accesso consentito.")
+        return username
+    else:
+        print("Credenziali errate.")
+        return None
+
+def ricerca_utenti():
+    print('da fare')
+    
+def aggiungi_contatti():
+    print('da fare')
+    
+def impostazione_dnd():
+    print('da fare')
+    
+def invia_mess():
+    print('da fare')
+
+def leggi_mess():
+    print('da fare')
